a4aef1e2e8fa4973cc5608226497af4ea410d93d
Hong Ly <7e12345678@gmail.com>
2026-07-14 20:57:39 +0700

    Update website

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,8 +1,14 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI!;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 export default async function connectDB() {
+  console.log("MONGODB_URI exists:", !!MONGODB_URI);
+
+  if (!MONGODB_URI) {
+    throw new Error("MONGODB_URI is missing");
+  }
+
   if (mongoose.connection.readyState >= 1) {
     return;
   }
